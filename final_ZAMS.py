e899461d83e5066eb97d1e590067b0c7445694c2
Gabriel-p <gabrielperren@gmail.com>
2013-12-16 16:23:42 -0300

    change contour_level name + add code for obtaining isoch parts (not functioning)

--- final_ZAMS.py
+++ final_ZAMS.py
@@ -61,7 +61,7 @@ def intrsc_values(col_obsrv, mag_obsrv, e_bv, dist_mod):
 manual_accept = ['BSDL654', 'BSDL761', 'BSDL779']
                    
 
-def contour_levels(fine_tune, cluster, x, y, kde):
+def clust_main_seq(fine_tune, cluster, x, y, kde):
     '''This is the central function. It generates the countour plots around the
     cluster members. The extreme points of these contour levels are used to trace
     the ZAMS fiducial line for each cluster.
@@ -388,12 +388,45 @@ data_all/cumulos-datos-fotometricos/'
             
             # Call the function that returns the sequence determined by the two
             # points further from each other in each contour level.
-            sequence, manual_levels, y_lim = contour_levels(fine_tune, cluster,
+            sequence, manual_levels, y_lim = clust_main_seq(fine_tune, cluster,
                                                             x, y, kde)
         
             # If the contour points returns an empty list don't attempt to
             # plot the polynomial fit.
             if sequence[0]:
+
+                # Obtain the sequence's fitting polinome.
+                poli_order = 3 # Order of the polynome.
+                poli = np.polyfit(sequence[1], sequence[0], poli_order)
+                y_pol = np.linspace(min(sequence[1]), max(sequence[1]), 50)
+                p = np.poly1d(poli)
+                x_pol = [p(i) for i in y_pol]
+
+                # Trim the interpolated sequence to the range in y axis.
+                y_pol_trim, x_pol_trim = zip(*[(ia,ib) for (ia, ib) in \
+                zip(y_pol,x_pol) if y_lim[0] <= ia <= y_lim[1]])
+                
+                # Store the interpolated trimmed sequence obtained for this
+                # cluster in final list.
+                clust_zams.append([x_pol_trim, y_pol_trim])
+                # Also store the parameters associated with this cluster.
+                clust_zams_params.append([cluster, cl_e_bv, cl_age, cl_feh,
+                                          cl_dmod])
+            else:
+                x_pol_trim, y_pol_trim = [], []
+
+        
+            # Write interpolated sequence to output file.
+            write_seq_file(out_dir, cluster, x_pol_trim, y_pol_trim)
+            
+            
+            # Call the function that returns the evolved part of the isochrone
+            # for this cluster.
+            isoch_seq, y_lim_iso = get_isoch_seq(cluster, x, y, kde)
+        
+            # If the contour points returns an empty list don't attempt to
+            # plot the polynomial fit.
+            if isoch_seq[0]:
 
                 # Obtain the sequence's fitting polinome.
                 poli_order = 3 # Order of the polynome.
@@ -479,6 +512,60 @@ def final_zams(clust_zams, clust_zams_params, m_rang):
 
     return ages_s, names_s, names_feh_s, final_zams_poli_s, zx_pol, zy_pol
     
+
+def add_isoch(clust_zams, clust_zams_params, m_rang):
+    '''
+    '''
+    
+    # This list holds the names and tuning parameters for those clusters that
+    # need it.
+    # 1st sub-list: Names of clusters
+    # 2nd: values to generate levels with np.arange()
+    # 3rd: y_min and y_max. Range where sequence will be interpolated.
+    # 4th: min level value to accept and min level number to accept.
+    add_isoch_list = [\
+    '']
+    
+    f_t_ylim = [\
+    [0.5, 3.2], [1., 2.6], [1.4, 4.], [3.2, 5.2], [3., 4.8], [2.2, 5.], \
+    [1.8, 2.7], [2., 4.], [2., 3.], [1., 3.4], [2.4, 3.6], [2., 4.], \
+    [0., 1.2], [-0.3, 1.2], [0.8, 1.8], [1.6, 2.8], [2., 3.2], [2., 4.5], \
+    [1.8, 5.], [2., 6.4], [1., 2.8], [2.8, 4.4]]
+    
+    f_t_level = [\
+    [], [], [-0.1, 0.], [], [], [-0.1, 1], \
+    [], [], [], [-0.1, 0.], [-0.1, 2], [-0.1, -1.], \
+    [], [-0.1, 0.], [-0.1, 1], [], [-0.1, 0.], [-0.1, 1.], \
+    [], [-0.1, 2.], [], []]
+    
+    f_t_range = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
+                 [], [], [], [], [], []]
+    
+    fine_tune_list = [f_t_names, f_t_range, f_t_ylim, f_t_level]
+
+    if fine_tune == True and cluster in fine_tune_list[0]:
+        indx = fine_tune_list[0].index(cluster)
+        if fine_tune_list[1][indx]:
+            manual_levels = np.arange(fine_tune_list[1][indx][0],
+                                      fine_tune_list[1][indx][1],\
+                                      fine_tune_list[1][indx][2])
+        else:
+            manual_levels = np.array([])
+        y_min, y_max = fine_tune_list[2][indx][0], fine_tune_list[2][indx][1]
+        lev_min, lev_num = fine_tune_list[3][indx] if fine_tune_list[3][indx] \
+        else [-0.1, -1]
+    else:
+        manual_levels = np.array([])
+        y_min, y_max = -10., 10.
+        lev_min, lev_num = 0.1, 1
+
+    # This list will hold the points obtained through the contour curves,
+    # the first sublist are the x coordinates of the points and the second
+    # the y coordinates.
+    sequence = [[], []]
+        
+    
+    return sequence, manual_levels, y_lim
 
 
 print '\nPlotting sequences by metallicity interval'
